5c2032fa96ed6b8af32e4396362dbf3da7b42b61
Annie Tallund <annie.tallund@arm.com>
2024-10-02 10:06:48 +0200

    Change output parser

--- .github/workflows/parse-output.py
+++ .github/workflows/parse-output.py
@@ -5,8 +5,11 @@ acl = sys.argv[2]
 results_sep = "---------------------------------------------------------"
 openblas = openblas.split(results_sep, 1)[-1]
 openblas_cpu_time_total = openblas.split("Self CPU time total: ", 1)[-1]
+openblas_cpu_time = openblas_cpu_time_total.split("ms", 1)[0]
+
 acl = acl.split(results_sep, 1)[-1]
 acl_cpu_time_total = acl.split("Self CPU time total: ", 1)[-1]
+acl_cpu_time = acl_cpu_time_total.split("ms", 1)[0]
 
 print("                         ------------------        OpenBLAS results       ------------------")
 print(openblas)
@@ -15,4 +18,4 @@ print("                         ------------------  Arm Compute Library results 
 print(acl)
 print("\n")
 print("                         ------------------  Self CPU time total change   ------------------")
-print(f"Self CPU time total went from {openblas_cpu_time_total} (OpenBLAS) to {acl_cpu_time_total} (ACL): {(float(acl_cpu_time_total.split("ms", 1)[0])/float(openblas_cpu_time_total.split("ms", 1)[0]))*100:.2f}% change")
+print(f"Self CPU time total went from {openblas_cpu_time_total} (OpenBLAS) to {acl_cpu_time} (ACL): {(float(acl_cpu_time)/float(openblas_cpu_time))*100:.2f}% change")
